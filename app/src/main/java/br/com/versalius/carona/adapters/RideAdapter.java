283b9d66562423330ea931d055c6f273f29d3f06
Giovanne Almeida <giovanne.almeida91@gmail.com>
2016-12-04 22:16:02 -0200

    Aprimorando layout de canronas disponíveis

--- app/src/main/java/br/com/versalius/carona/adapters/RideAdapter.java
+++ app/src/main/java/br/com/versalius/carona/adapters/RideAdapter.java
@@ -1,8 +1,10 @@
 package br.com.versalius.carona.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.media.Image;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,7 @@ import java.util.List;
 
 import br.com.versalius.carona.R;
 import br.com.versalius.carona.models.Ride;
+import br.com.versalius.carona.utils.PreferencesHelper;
 
 /**
  * Created by Giovanne on 03/12/2016.
@@ -40,17 +43,41 @@ public class RideAdapter extends RecyclerView.Adapter<RideAdapter.ViewHolder>{
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
         holder.tvDriverName.setText(list.get(position).getDriver().getFullName());
         holder.tvTime.setText(list.get(position).getDepartTimeString());
         holder.tvAvailableSits.setText(String.valueOf(list.get(position).getAvailableSits()));
         holder.tvOrigin.setText(list.get(position).getOrigin());
         holder.tvDestination.setText(list.get(position).getFullDestination());
         holder.ivProfile.setImageResource(list.get(position).getDriver().getPhoto());
+        if(list.get(position).getAvailableSits() == 0){
+            holder.btGetRide.setEnabled(false);
+            holder.btGetRide.setText("Cheia");
+            holder.btGetRide.setTextColor(Color.BLACK);
+            holder.btGetRide.setBackgroundColor(Color.GRAY);
+        }
         holder.btGetRide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context,"FALTA FAZER",Toast.LENGTH_LONG).show();
+                if(PreferencesHelper.getInstance(context).load("currentRideId").isEmpty()) {
+                    holder.btGetRide.setText("Sair");
+                    holder.btGetRide.setBackgroundColor(ContextCompat.getColor(context, R.color.colorSecondary));
+                    try {
+                        PreferencesHelper.getInstance(context).save("currentRideId", String.valueOf(list.get(position).getId()));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else if(PreferencesHelper.getInstance(context).load("currentRideId").equals(String.valueOf(list.get(position).getId()))) {
+                    holder.btGetRide.setText("Entrar");
+                    holder.btGetRide.setBackgroundColor(ContextCompat.getColor(context, R.color.colorAccent));
+                    try {
+                        PreferencesHelper.getInstance(context).save("currentRideId", "");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Toast.makeText(context,"Você já está numa carona",Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
